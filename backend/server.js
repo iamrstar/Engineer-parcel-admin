d38b5a208f4716f2108f0b358d22193fb3b2b8dc
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-09-04 17:02:44 +0530

    Update server.js
    updated trackhistory

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   "http://localhost:5173", // local dev
   "https://engineer-parcel-admin.netlify.app",
   "https://engineer-parcel-admin.vercel.app",
-    "https://ep.engineersparcel.in"              
+   "https://ep.engineersparcel.in" 
 ]
 
 app.use(
@@ -42,7 +42,7 @@ app.get("/api/health", (req, res) => {
 mongoose
   .connect(
     process.env.MONGODB_URI 
-        )
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err))
 
@@ -57,10 +57,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/bookings", bookingRoutes)
 app.use("/api/pincodes", pincodeRoutes)
 app.use("/api/coupons", couponRoutes)
-app.use("/api/manual-bookings", manualBookingRoute);
+app.use("/api/manual-bookings", manualBookingRoute);  
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {  
   console.log(`Server running on port ${PORT}`)
 })
