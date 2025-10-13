7012eaad0c692d4e43d0afa5606d465aeaab7aad
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-10-14 00:38:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",  // 👈 for local dev only
+        target: "https://admin-api.engineersparcel.in",  // 👈 for local dev only
         changeOrigin: true,
         secure: false,
       },
