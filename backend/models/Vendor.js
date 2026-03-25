3b04b17ff620d633eb29f916caaa348491f1f993
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2026-03-25 20:42:56 +0530

    Create Vendor.js

---
+++ backend/models/Vendor.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const vendorSchema = new mongoose.Schema({
+    vendorId: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    address2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    pincode: {
+        type: String,
+        required: true,
+    },
+    landmark: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Vendor", vendorSchema);
